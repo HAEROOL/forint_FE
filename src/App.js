40cdb2d970dee36ca7670f640576abb1d1ae2881
HAEROOL <gofn080776@gmail.com>
2022-02-13 21:03:52 +0900

    [#2] feat: 로그인 페이지 레이아웃 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoginPage from './page/LoginPage';
 import MainPage from './page/MainPage';
 
 
 function App() {
   return (
-    <MainPage/>
+    // <MainPage/>
+    <LoginPage/>
   );
 }
 
