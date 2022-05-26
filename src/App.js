b44e79b584253e467a749a160975d2a7402ed0ab
HAEROOL <gofn080776@gmail.com>
2022-05-26 16:02:59 +0900

    [#18] style:로그인 페이지 스타일 변경

--- src/App.js
+++ src/App.js
@@ -41,16 +41,16 @@ function App() {
     <Header/>
     <Routes>
     <Route path='/' element={<MainPage/>}/>
-    {/* <Route path='/login' element={<LoginPage/>}/>
-    <Route path='/lookout' element={<LookOutPage/>}/>
+    <Route path='/login' element={<LoginPage/>}/>
+    {/* <Route path='/lookout' element={<LookOutPage/>}/>
     <Route path='/signup' element={<SignPage/>}/>
     <Route path='/fontcreate/*' element={<FontMakePage/>}>
       <Route index element={<FirsrtPannel/>}/>
       <Route path="second" element={<SecondPannel/>}/>
       <Route path="third" element={<ThirdPannel/>}/>
       <Route path="fourth" element={<FourthPannel/>}/>
-    </Route>
-    <Route path='/mypage' element={<MyPage/>}/> */}
+    </Route> */}
+    <Route path='/mypage' element={<MyPage/>}/>
   </Routes>
   </StyledContatiner>
     
---
+++ src/staticColor.js
@@ -0,0 +1,4 @@
+export const COLOR = {
+    lightgray: 'rgb(191,191,191)',
+    yellow: 'rgb(255,217,102)'
+}
