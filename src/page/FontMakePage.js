916e7d386c5e11b857550b07fa5e173c5ff2508b
HAEROOL <gofn080776@gmail.com>
2022-05-31 21:23:49 +0900

    [#18] feat: stepbar 구현

--- src/page/FontMakePage.js
+++ src/page/FontMakePage.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router";
 import styled from "styled-components";
 import FifthPannel from "../component/FontMake/FifthPannel_DisplayFont";
 import FinalPannel from "../component/FontMake/FinalPannel";
@@ -13,13 +13,15 @@ import Header from "../component/Public/Header";
 const PageWrapper = styled.div`
 `
 const FontMakePage = () => {
+    const location = useLocation()
+    const STEP_ARRAY = ['/fontcreate', '/fontcreate/second','/fontcreate/fourth']
+    console.log(STEP_ARRAY.indexOf(location.pathname), location.pathname)
     return(
         <PageWrapper>
-            <StepBar step={1}/>
+            <StepBar step={STEP_ARRAY.indexOf(location.pathname)+1}/>
             <Routes>
                 <Route path="/" element={<FirsrtPannel/>}/>
                 <Route path="/second" element={<SecondPannel/>}/>
-                {/* <Route path="/third" element={<ThirdPannel/>}/> */}
                 <Route path="/fourth" element={<FourthPannel/>}/>
                 <Route path="/fifth" element={<FifthPannel/>}/>
                 <Route path="/final" element={<FinalPannel/>}/>
